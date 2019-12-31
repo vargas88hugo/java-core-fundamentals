92d222d0a6f8b6585c9edbb5d5b406ad26143ed0
Hugo Vargas <vargas88hugo@gmail.com>
2019-12-30 20:01:06 -0500

    [Feature] Created program with hash map implementations. It contains nested maps and simulate little game navigation

---
+++ java-collections/com/map01/Location.java
@@ -0,0 +1,31 @@
+package com.map01;
+
+import java.util.*;
+
+public class Location {
+    private final int locationID;
+    private final String description;
+    private final Map<String, Integer> exits;
+
+    public Location(int locationID, String description) {
+        this.locationID = locationID;
+        this.description = description;
+        this.exits = new HashMap<String, Integer>();
+        this.exits.put("Q", 0);
+    }
+
+    public void addExit(String direction, int location) {
+        exits.put(direction, location);
+    }
+    public int getLocationID() {
+        return locationID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Map<String, Integer> getExits() {
+        return new HashMap<String, Integer>(exits);
+    }
+}
